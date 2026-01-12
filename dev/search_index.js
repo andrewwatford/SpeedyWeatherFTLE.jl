1ec95421bf71b7bb3d04ae889ed3704815f474e1
Documenter.jl <documenter@juliadocs.github.io>
2026-01-12 16:56:03 +0000

    build based on 4afeaba

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#SpeedyWeatherFTLE","page":"SpeedyWeatherFTLE","title":"SpeedyWeatherFTLE","text":"Documentation for SpeedyWeatherFTLE.","category":"section"}]
+}
