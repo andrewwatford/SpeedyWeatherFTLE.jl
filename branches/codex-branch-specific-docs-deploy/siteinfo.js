8a43ab2b3ee6bef22caae00e40fd49b4b9f9cd56
Documenter.jl <documenter@juliadocs.github.io>
2026-07-03 13:42:14 +0000

    build docs for codex-branch-specific-docs-deploy based on 11bb58b

---
+++ branches/codex-branch-specific-docs-deploy/siteinfo.js
@@ -0,0 +1,3 @@
+var DOCUMENTER_CURRENT_VERSION = "codex-branch-specific-docs-deploy";
+var DOCUMENTER_STABLE = "codex-branch-specific-docs-deploy";
+var DOCUMENTER_IS_DEV_VERSION = true;
---
+++ branches/versions.js
@@ -0,0 +1,4 @@
+var DOC_VERSIONS = [
+  "codex-branch-specific-docs-deploy",
+];
+var DOCUMENTER_NEWEST = "codex-branch-specific-docs-deploy";
